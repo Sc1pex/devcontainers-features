24c8731e00f2245cf69a7bfebe13353ddcd3fe7d
Sc1pex <scipex.dev@gmail.com>
2024-09-16 17:22:40 +0300

    add python devcontainer dependency

--- src/meson/install.sh
+++ src/meson/install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Activating feature 'meson'"
-
-echo "Installing dependencies"
-apt-get install python3 python3-pip python3-setuptools python3-wheel ninja-build
-
 echo "Installing meson"
 pip3 install meson
